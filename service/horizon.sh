562152fd8a3c2e182a4264a2b4fa92e48d4b621a
kompiangg <kompiang.sukadharma@gmail.com>
2020-02-05 23:25:00 +0800

    update

--- service/horizon.sh
+++ service/horizon.sh
@@ -13,8 +13,28 @@ yum install openstack-dashboard -y
 
 echo
 echo "LOG: configure"
-sed -i -e 's/OPENSTACK_HOST.*=.*/OPENSTACK_HOST = "$CONTROLLER_PROVIDER_IP"/' /etc/openstack_dashboard/local_settings
-sed -i -e "s/ALLOWED_HOSTS.*=.*/ALLOWED_HOSTS = ['*']/" /etc/openstack_dashboard/local_settings
-cat >> /etc/openstack_dashboard/local_settings << EOF
+sed -i -e "s/OPENSTACK_HOST.*=.*/OPENSTACK_HOST = '$CONTROLLER_PROVIDER_IP'/" /etc/openstack-dashboard/local_settings
+sed -i -e "s/ALLOWED_HOSTS.*=.*/ALLOWED_HOSTS = ['*']/" /etc/openstack-dashboard/local_settings
+cat >> /etc/openstack-dashboard/local_settings << EOF
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 EOF
+
+sed -i -e 's/#* *#CACHES .*= {.*/CACHES = {/' /etc/openstack-dashboard/local_settings
+sed -i -e "s/#*.*'default'.*: {.*/    'default': {/" /etc/openstack-dashboard/local_settings
+sed -i -e "s/#*.*'BACKEND'.*:.*/        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',/" /etc/openstack-dashboard/local_settings
+sed -i -e "s/#*.*'LOCATION'.*:.*/        'LOCATION': '$CONTROLLER_PROVIDER_IP:1121',/" /etc/openstack-dashboard/local_settings
+sed -i -e "s/#*.*}.*,/    }/" /etc/openstack-dashboard/local_settings
+sed -i -e "s/#}/}/" /etc/openstack-dashboard/local_settings
+sed -i -e 's/OPENSTACK_KEYSTONE_DEFAULT_ROLE = "_member_"/OPENSTACK_KEYSTONE_DEFAULT_ROLE = "user"/' /etc/openstack-dashboard/local_settingss
+cat >> /etc/openstack-dashboard/local_settings << EOF
+OPENSTACK_API_VERSIONS = {
+    "identity": 3,
+    "image": 2,
+    "volume": 2,
+}
+EOF
+sed -i -e 's/* *OPENSTACK_KEYSTONE_DEFAULT_ROLE .*=.*/OPENSTACK_KEYSTONE_DEFAULT_ROLE = "user"/' /etc/openstack-dashboard/local_settings
+cat >> /etc/httpd/conf.d/openstack-dashboard.conf << EOF
+WSGIApplicationGroup %{GLOBAL}
+EOF
+systemctl restart httpd.service memcached.service
