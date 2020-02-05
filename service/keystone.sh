8e66f8e8c0ab8c55cc1417bab1ceb9fd87b7c68b
kompiangg <kompiang.sukadharma@gmail.com>
2020-02-05 20:36:28 +0800

    First Commit

---
+++ service/keystone.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo 'Please run as root'
+    exit
+fi
+
+source ../network/network_var.sh
+
+echo
+echo "LOG: create keystone database"
+mysql -u root -p$DBROOT_PASS -e "CREATE DATABASE keystone;"
+mysql -u root -p$DBROOT_PASS -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY '$KEYSTONEDB_PASS';"
+mysql -u root -p$DBROOT_PASS -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '$KEYSTONEDB_PASS';"
+mysql -u root -p$DBROOT_PASS -e "FLUSH PRIVILEGES;"
+
+echo
+echo "LOG: install package"
+yum install openstack-keystone httpd mod_wsgi -y
+
+echo
+echo "LOG: edit configuration"
+crudini --set /etc/keystone/keystone.conf database connection mysql+pymysql://keystone:$KEYSTONEDB_PASS@$CONTROLLER_PROVIDER_IP/keystone
+crudini --set /etc/keystone/keystone.conf token provider fernet
+
+echo
+echo "LOG: populate database"
+su -s /bin/sh -c "keystone-manage db_sync" keystone
+
+echo
+echo "LOG: init fernet key"
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+
+echo
+echo "LOG: bootstrap keystone"
+keystone-manage bootstrap --bootstrap-password $ADMIN_PASS \
+  --bootstrap-admin-url http://$CONTROLLER_PROVIDER_IP/v3/ \
+  --bootstrap-internal-url http://$CONTROLLER_PROVIDER_IP/v3/ \
+  --bootstrap-public-url http://$CONTROLLER_PROVIDER_IP/v3/ \
+  --bootstrap-region-id RegionOne
+
+echo
+echo "LOG: configure httpd"
+cat >> /etc/httpd/conf/httpd.conf << EOF
+ServerName controller
+EOF
+
+echo
+echo "LOG: create link"
+ln -s /usr/share/keystone/wsgi-keystone.conf /etc/httpd/conf.d/
+
+echo
+echo "LOG: start"
+systemctl enable httpd.service
+systemctl start httpd.service
+
+echo
+echo "LOG: create environment file"
+cat > ../admin_rc << EOF
+export OS_USERNAME=admin
+export OS_PASSWORD=$ADMIN_PASS
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+EOF
+source ../admin_rc
+
+echo
+echo "LOG: verify"
+openstack --os-auth-url http://controller:35357/v3 \
+  --os-project-domain-name Default --os-user-domain-name Default \
+  --os-project-name admin --os-username admin token issue
+
+echo
+echo "LOG: create service project"
+openstack project create --domain default --description "Service Project" service
---
+++ service/nova-compute.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo 'Please run as root'
+    exit
+fi
+
+source ../network/network_var.sh
+
+echo
+echo "LOG: install package"
+yum install openstack-nova-compute -y
+
+echo
+echo "LOG: configure"
+crudini --set /etc/nova/nova.conf DEFAULT enabled_apis osapi_compute,metadata
+crudini --set /etc/nova/nova.conf DEFAULT transport_url rabbit://openstack:$RABBIT_PASS@$CONTROLLER_PROVIDER_IP
+crudini --set /etc/nova/nova.conf DEFAULT my_ip $COMPUTE_PROVIDER_IP
+crudini --set /etc/nova/nova.conf DEFAULT use_neutron True
+crudini --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
+
+crudini --set /etc/nova/nova.conf vnc enabled True
+crudini --set /etc/nova/nova.conf vnc server_listen 0.0.0.0
+crudini --set /etc/nova/nova.conf vnc server_proxyclient_address $COMPUTE_PROVIDER_IP
+crudini --set /etc/nova/nova.conf vnc novncproxy_base_url http://$CONTROLLER_PROVIDER_IP:6080/vnc_auto.html
+
+crudini --set /etc/nova/nova.conf glance api_servers http://$CONTROLLER_PROVIDER_IP:9292
+crudini --set /etc/nova/nova.conf oslo_concurrency lock_path /var/lib/nova/tmp
+
+crudini --set /etc/nova/nova.conf placement os_region_name RegionOne
+crudini --set /etc/nova/nova.conf placement project_domain_name Default
+crudini --set /etc/nova/nova.conf placement project_name service
+crudini --set /etc/nova/nova.conf placement auth_type password
+crudini --set /etc/nova/nova.conf placement user_domain_name Default
+crudini --set /etc/nova/nova.conf placement auth_url http://$CONTROLLER_PROVIDER_IP:5000/v3
+crudini --set /etc/nova/nova.conf placement username placement
+crudini --set /etc/nova/nova.conf placement password PLACEMENT_PASS
+
+crudini --set /etc/nova/nova.conf api auth_strategy keystone
+
+crudini --set /etc/nova/nova.conf keystone_authtoken auth_url http://$CONTROLLER_PROVIDER_IP:5000/v3
+crudini --set /etc/nova/nova.conf keystone_authtoken memcached_servers $CONTROLLER_PROVIDER_IP:11211
+crudini --set /etc/nova/nova.conf keystone_authtoken auth_type password
+crudini --set /etc/nova/nova.conf keystone_authtoken project_domain_name Default
+crudini --set /etc/nova/nova.conf keystone_authtoken user_domain_name Default
+crudini --set /etc/nova/nova.conf keystone_authtoken project_name service
+crudini --set /etc/nova/nova.conf keystone_authtoken username nova
+crudini --set /etc/nova/nova.conf keystone_authtoken password NOVA_PASS
+
+crudini --set /etc/nova/nova.conf libvirt virt_type qemu
+
+echo
+echo "LOG: start service"
+systemctl enable libvirtd.service openstack-nova-compute.service
+systemctl start libvirtd.service openstack-nova-compute.service
