71b80c299438d2f9d8e6ddc1ca8f69f2b567ee19
prasta <52882935+prastamaha@users.noreply.github.com>
2020-02-06 13:01:11 +0800

    fix conditional variable

--- service/nova-add-compute.sh
+++ service/nova-add-compute.sh
@@ -7,16 +7,15 @@ fi
 
 echo 'WARNING: this script is run when the nova service on the compute node is running, make sure you have installed the nova service on the controller node before running this script'
 
-run='Y'
-read -p 'Are you sure to continue? [Y/n]' $run
+read -p 'Are you sure to continue? [Y/n]' run
 
-if [ $run -eq 'Y' ]; then
+if [ "$run" = "Y" ]; then
     echo
     echo "LOG: Add compute node to cell database Run this on controller node"
     source ../admin_rc
     openstack compute service list --service nova-compute
     su -s /bin/sh -c "nova-manage cell_v2 discover_hosts --verbose" nova
-elif [ $run = 'n' ]; then
+elif [ "$run" = "n" ]; then
     echo 'Exited from Nova add compute'
     exit
 else
