0d58e261d55a7d8b9726265a2e63b4582c6665b4
kompiangg <kompiang.sukadharma@gmail.com>
2020-02-12 07:48:54 +0800

    fix typo

--- service/horizon.sh
+++ service/horizon.sh
@@ -13,18 +13,17 @@ yum install openstack-dashboard -y
 
 echo
 echo "LOG: configure"
-sed -i -e "s/OPENSTACK_HOST.*=.*/OPENSTACK_HOST = '$CONTROLLER_PROVIDER_IP'/" /etc/openstack-dashboard/local_settings
+sed -i -e "s/OPENSTACK_HOST.*=.*/OPENSTACK_HOST = '$CONTROLLER_MANAGEMENT_IP'/" /etc/openstack-dashboard/local_settings
 sed -i -e "s/ALLOWED_HOSTS.*=.*/ALLOWED_HOSTS = ['*']/" /etc/openstack-dashboard/local_settings
 cat >> /etc/openstack-dashboard/local_settings << EOF
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 EOF
-
 #sed -i -e 's/#* *#CACHES .*= {.*/CACHES = {/' /etc/openstack-dashboard/local_settings
 #sed -i -e "s/#*.*'default'.*: {.*/    'default': {/" /etc/openstack-dashboard/local_settings
 sed -i -e "167s/#*.*'BACKEND'.*:.*/        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',/" /etc/openstack-dashboard/local_settings
-sed -i -e "167 a         'LOCATION': '$CONTROLLER_PROVIDER_IP:1121'," /etc/openstack-dashboard/local_settings
-#sed -i -e "s/#*.*'LOCATION'.*:.*/        'LOCATION': '$CONTROLLER_PROVIDER_IP:11211',}}/" /etc/openstack-dashboard/local_settings
-sed -i -e "168s/'LOCATION':.*/        'LOCATION': '$CONTROLLER_PROVIDER_IP:11211',/" /etc/openstack-dashboard/local_settings
+sed -i -e "167 a 'LOCATION': '$CONTROLLER_MANAGEMENT_IP:11211'," /etc/openstack-dashboard/local_settings
+#sed -i -e "s/#*.*'LOCATION'.*:.*/        'LOCATION': '$CONTROLLER_MANAGEMENT_IP:11211',}}/" /etc/openstack-dashboard/local_settings
+sed -i -e "168s/'LOCATION':.*/        'LOCATION': '$CONTROLLER_MANAGEMENT_IP:11211',/" /etc/openstack-dashboard/local_settings
 sed -i -e "169s/#*.*}.*,/    }/" /etc/openstack-dashboard/local_settings
 #sed -i -e "s/#}/}/" /etc/openstack-dashboard/local_settings
 sed -i -e 's/OPENSTACK_KEYSTONE_DEFAULT_ROLE = "_member_"/OPENSTACK_KEYSTONE_DEFAULT_ROLE = "user"/' /etc/openstack-dashboard/local_settings
